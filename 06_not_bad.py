b193dd2a145ea90e7f493f77f419497a8e13af0d
Elismar Luz <elismar@elismarluz.com>
2020-06-29 10:05:22 -0300

    Feito e com 3 novos testes

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -6,12 +6,13 @@ substrings 'not' e 'bad'. Se 'bad' aparecer depois
 de 'not', troque todo o trecho entre 'not' e 'bad'
 por 'good' e retorne a string resultante.
 
-Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
+Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'zxzx
 """
+import re
+
 
 def not_bad(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    return re.sub(r'(not).*?(bad)', r'good', s)
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
@@ -39,3 +40,6 @@ if __name__ == '__main__':
     test(not_bad, 'This dinner is not that bad!', 'This dinner is good!')
     test(not_bad, 'This tea is not hot', 'This tea is not hot')
     test(not_bad, "It's bad yet not", "It's bad yet not")
+    test(not_bad, "Bad but not so bad", "Bad but good")
+    test(not_bad, "E se tiver not bad e depois um not bad novamente", "E se tiver good e depois um good novamente")
+    test(not_bad, "E se tiver not not bad", "E se tiver good")
